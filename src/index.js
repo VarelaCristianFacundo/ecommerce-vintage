17ddc93a8c237df3093ac2a67c594fd5223d3415
Cristian Facundo Varela <cvarelagarcia@gmail.com>
2022-05-10 13:24:21 -0300

    modificaciones para el deploy a github pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { CartContextProvider } from './store/cart-context';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <CartContextProvider>
         <App />
       </CartContextProvider>
